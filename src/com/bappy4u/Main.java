4120deeb2017228fd665fe243e52ff39e424f8bc
Najmul H. Bappy <http.bappy@gmail.com>
2022-03-18 02:59:02 +0600

    OODA Objectivilla exercise of head first OODA

--- src/com/bappy4u/Main.java
+++ src/com/bappy4u/Main.java
@@ -16,8 +16,27 @@ public class Main {
 
 
         Airplane biplane = new Airplane();
-
         biplane.setSpeed(212);
+        System.out.println(biplane.getSpeed());
+
+        Jet boeing = new Jet();
+        boeing.setSpeed(422);
+        System.out.println(boeing.getSpeed());
+
+
+        int i = 0;
+
+        while (i<4){
+            boeing.accelerate();
+            System.out.println(boeing.getSpeed());
+            if(boeing.getSpeed()>5000){
+               biplane.setSpeed(biplane.getSpeed() * 2);
+            }
+            else{
+                boeing.accelerate();
+            }
+            i++;
+        }
         System.out.println(biplane.getSpeed());
     }
 }
