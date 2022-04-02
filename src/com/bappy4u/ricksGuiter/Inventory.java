b683b3f30a4f45b0754607d53b9d5f7c6df8cda9
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-03 00:33:42 +0600

    Guitar inventory added

---
+++ src/com/bappy4u/ricksGuiter/Inventory.java
@@ -0,0 +1,16 @@
+package com.bappy4u.ricksGuiter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Inventory {
+    private List guitars;
+    public Inventory(){
+        guitars = new LinkedList();
+    }
+
+    public void addGuitar(String serialNumber, double price, String builder, String model, String type, String backWood, String topWood){
+        Guitar  guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+        guitars.add(guitar);
+    }
+}
