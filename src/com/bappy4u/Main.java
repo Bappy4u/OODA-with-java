da9adcad2af006d5e7041267690f7f2e02de6de5
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-26 00:43:19 +0600

    updated

--- src/com/bappy4u/Main.java
+++ src/com/bappy4u/Main.java
@@ -1,48 +1,13 @@
 package com.bappy4u;
 
+import com.bappy4u.graph.graph;
 import com.bappy4u.ricksGuiter.Guitar;
 
 public class Main {
 
     public static void main(String[] args) {
-        DataValidation dataValidation = new DataValidation();
+        graph graph = new graph();
 
-        boolean isPasswordValid = dataValidation.isPasswordValid("Password", "password");
-
-        if (isPasswordValid){
-            System.out.println("The password is matched");
-        }
-        else{
-            System.out.println("Password doesn't match.");
-        }
-
-
-        Airplane biplane = new Airplane();
-        biplane.setSpeed(212);
-        System.out.println(biplane.getSpeed());
-
-        Jet boeing = new Jet();
-        boeing.setSpeed(422);
-        System.out.println(boeing.getSpeed());
-
-
-        int i = 0;
-
-        while (i<4){
-            boeing.accelerate();
-            System.out.println(boeing.getSpeed());
-            if(boeing.getSpeed()>5000){
-               biplane.setSpeed(biplane.getSpeed() * 2);
-            }
-            else{
-                boeing.accelerate();
-            }
-            i++;
-        }
-        System.out.println(biplane.getSpeed());
-
-        Guitar guitar = new Guitar("3",4.16,"Yamaha","ym11","rock","Segun","kathal");
-
-        System.out.println("Guitar Builder: " + guitar.getBuilder() + " Model: "+guitar.getModel());
+        graph.addNode("label");
     }
 }
--- src/com/bappy4u/graph/graph.java
+++ src/com/bappy4u/graph/graph.java
@@ -1,2 +1,47 @@
-package com.bappy4u.graph;public class graph {
+package com.bappy4u.graph;
+
+
+import java.util.*;
+
+public class graph {
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Node>> adjancencyList = new HashMap<>();
+    private class Node{
+        String label;
+        public Node(String label){
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+    public graph(){
+    }
+    public void addNode(String label){
+            var node = new Node(label);
+            this.nodes.putIfAbsent(label, node);
+            this.adjancencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdges(String from, String to){
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+        if (fromNode==null || toNode==null){
+            throw new IllegalArgumentException();
+        }
+
+        this.adjancencyList.get(fromNode).add(toNode);
+    }
+
+    public void print(){
+        for (var source: adjancencyList.keySet()){
+            var targets = adjancencyList.get(source);
+
+            if (!targets.isEmpty()){
+                System.out.println(source+" is connected to "+targets);
+            }
+        }
+    }
 }
